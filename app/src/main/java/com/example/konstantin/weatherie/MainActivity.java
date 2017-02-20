be2922b15171565c481992f445f3ecf5f99375dd
DESKTOP-FAADA6L\Konstantin <k00190580@student.lit.ie>
2017-02-20 21:23:44 +0000

    no message

--- app/src/main/java/com/example/konstantin/weatherie/MainActivity.java
+++ app/src/main/java/com/example/konstantin/weatherie/MainActivity.java
@@ -881,13 +881,9 @@ String weekDay ="";
 
     public WeatherRecyclerAdapter getAdapter(int id) {
         WeatherRecyclerAdapter weatherRecyclerAdapter;
-        if (id == 0) {
+
             weatherRecyclerAdapter = new WeatherRecyclerAdapter(this, longTermTodayWeather);
-        } else if (id == 1) {
-            weatherRecyclerAdapter = new WeatherRecyclerAdapter(this, longTermTomorrowWeather);
-        } else {
-            weatherRecyclerAdapter = new WeatherRecyclerAdapter(this, longTermWeather);
-        }
+        
         return weatherRecyclerAdapter;
     }
 
